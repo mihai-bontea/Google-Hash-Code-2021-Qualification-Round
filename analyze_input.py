8d0d40d34aeecea2717546763b6aee57caadba07
mihai-bontea <mihaibontea@protonmail.com>
2021-09-19 16:42:40 +0300

    fixed bug

--- analyze_input.py
+++ analyze_input.py
@@ -1,27 +1,42 @@
 input_files = ["a.txt", "b.txt", "c.txt", "d.txt", "e.txt", "f.txt"]
 
-class Car:
+class Street:
     def __init__(self, info_str):
         info = info_str.strip().split(" ")
-        self.streets = info[1:]
+        self.intersect = (int(info[0]), int(info[1]))
+        self.name = info[2]
+        self.length = info[3]
 
-    def min_finish_time(self, street_len):
-        time = 0
-        for street in self.streets[1:]:
-            time += street_len[street]
-        return time
+
+def get_street_by_name(str_list, name):
+    for street in str_list:
+        if street.name == name:
+            return street
+    return None
+
+  
+class Car:
+    def __init__(self, info_str, streets):
+        info = info_str.strip().split(" ")
+        street_names = info[1:]
+        self.streets = []
+        for street_name in street_names:
+            self.streets.append(streets[street_name])
 
 
 class Component:
     def __init__(self, car):
         self.cars = [car]
-        self.streets = set(car.streets)
+        self.intersect = set()
+        for street in car.streets:
+            self.intersect.add(street.intersect[1])
 
     def add_car(self, car):
         self.cars.append(car)
         for street in car.streets:
-            self.streets.add(street)
+            self.intersect.add(street.intersect[1])
             
+
 for input_file in input_files:
     print("For input file " + input_file + ":")
     with open("input_files/" + input_file, "r") as fin:
@@ -35,52 +50,40 @@ for input_file in input_files:
         nr_cars = int(info[3])
         finish_bonus = int(info[4])
 
-        # Remember the length of each street
-        street_len = {}
+        # Reading the streets
+        streets = {}
         for _ in range(nr_streets):
-            info = fin.readline().strip().split(" ")
-            street_len[info[2]] = int(info[3])
+            info_str = fin.readline()
+            street = Street(info_str)
+            streets[street.name] = street
 
+        # Reading the cars
         cars = []
-        invalid_cars = 0
-        no_wait = 0
-        # Reading the path each car follows, counting cars that can't finish, if any
         for _ in range(nr_cars):
             info_str = fin.readline()
-            new_car = Car(info_str)
-            if new_car.min_finish_time(street_len) < total_time:
-                cars.append(new_car)
-            elif new_car.min_finish_time(street_len) == total_time:
-                cars.append(new_car)
-                no_wait += 1
-            else:
-                invalid_cars += 1
-
-        print("\tWe have " + str(invalid_cars) + " invalid cars and " + str(no_wait) + " cars that need perfect time") 
-
+            cars.append(Car(info_str, streets))
+            
         # Counting the number of unused streets, if any
-        streets = set()
+        used_streets = set()
         for car in cars:
             for street in car.streets:
-                streets.add(street)
-        print("\tWe have " + str(nr_streets - len(streets)) + " unused streets out of " + str(nr_streets))
+                used_streets.add(street.name)
+        print("\tWe have " + str(nr_streets - len(used_streets)) + " unused streets out of " + str(nr_streets))
 
         # Determining the number of connected components in the graph formed by the paths that the cars follow
         connected_components = []
         for car in cars:
-            any_street_appears = False
+            has_common_intersect = False
             for street in car.streets:
-                street_appears = False
                 for component in connected_components:
-                    if street in component.streets:
-                        street_appears = True
+                    if street.intersect[1] in component.intersect:
+                        has_common_intersect = True
                         component.add_car(car)
                         break
                     
-                if street_appears == True:
-                    any_street_appears = True
+                if has_common_intersect == True:
                     break
-            if any_street_appears == False:
+            if has_common_intersect == False:
                 connected_components.append(Component(car))
 
         print("\tWe have " + str(len(connected_components)) + " connected components")
