e743fb0f19b9213ef36e83021e0ff695c31df612
mihai-bontea <mihaibontea@protonmail.com>
2026-02-20 06:13:33 +0200

    adding basic solution

--- sol1/Data.h
+++ sol1/Data.h
@@ -1,6 +1,56 @@
 #pragma once
+#include <vector>
+#include <fstream>
+#include <unordered_map>
 
 struct Data
 {
+    int simulation_seconds, nr_intersections, nr_streets, nr_cars, score_per_car;
+    std::unordered_map<std::string, std::tuple<int,int,int>> street_info;
+    std::vector<std::vector<std::string>> incoming, car_paths;
+    std::unordered_map<std::string, int> street_usage;
 
+    explicit Data(const std::string& filename)
+    {
+        std::ifstream fin(filename);
+
+        fin >> simulation_seconds >> nr_intersections >> nr_streets >> nr_cars >> score_per_car;
+        incoming.resize(nr_intersections);
+        car_paths.resize(nr_cars);
+
+        for (int i = 0; i < nr_streets; ++i)
+        {
+            int start_int, end_int, length_in_sec;
+            std::string street_name;
+            fin >> start_int >> end_int >> street_name >> length_in_sec;
+            street_info[street_name] = {start_int, end_int, length_in_sec};
+            incoming[end_int].push_back(street_name);
+        }
+
+        for (int i = 0; i < nr_cars; ++i)
+        {
+            int nr_streets_in_path;
+            std::string street;
+
+            fin >> nr_streets_in_path;
+            while (nr_streets_in_path--)
+            {
+                fin >> street;
+                street_usage[street]++;
+                car_paths[i].push_back(street);
+            }
+        }
+    }
+
+    static void write_to_file(const std::string& filename, const std::map<int, std::vector<std::pair<std::string,int>>>& schedule)
+    {
+        std::ofstream fout(filename);
+        fout << schedule.size() << '\n';
+        for (const auto& [intersection_id, schedule_for_int] : schedule)
+        {
+            fout << intersection_id << '\n' << schedule_for_int.size() << '\n';
+            for (const auto& [street_name, seconds_green] : schedule_for_int)
+                fout << street_name << " " << seconds_green << '\n';
+        }
+    }
 };
--- sol1/main.cpp
+++ sol1/main.cpp
@@ -1,7 +1,46 @@
+#include <map>
+#include <array>
 #include <iostream>
+
+#include "Data.h"
+
+class BasicSolver
+{
+private:
+    const Data &data;
+public:
+    explicit BasicSolver(const Data &data)
+    : data(data)
+    {}
+
+    std::map<int, std::vector<std::pair<std::string,int>>> solve()
+    {
+        std::map<int, std::vector<std::pair<std::string,int>>> schedule;
+        for (const auto& [street_name, _] : data.street_usage)
+        {
+            int intersection = std::get<1>(data.street_info.at(street_name));
+            schedule[intersection].emplace_back(street_name, 1);
+        }
+
+        return schedule;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    const std::string in_prefix = "../../input_files/";
+    const std::string out_prefix = "../../output_files/sol1/";
+    const std::array<std::string, 6> input_files = {"a_example.in", "b_ocean.in", "c_checkmate.in",
+                                                    "d_daily_commute.in", "e_etoile.in", "f_forever_jammed.in"};
+
+    for (const auto& input_file : input_files)
+    {
+        Data data(in_prefix + input_file);
+        BasicSolver solver(data);
+        const auto result = solver.solve();
+
+        const auto out_filename = out_prefix + input_file.substr(0, (input_file.find('.'))) + ".out";
+        Data::write_to_file(out_filename, result);
+    }
     return 0;
 }
