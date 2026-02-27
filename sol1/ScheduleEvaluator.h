d574dac703c0c452e9062d33d22fbf142a4a7e89
mihai-bontea <mihaibontea@protonmail.com>
2026-02-27 11:42:25 +0200

    update

--- sol1/ScheduleEvaluator.h
+++ sol1/ScheduleEvaluator.h
@@ -3,6 +3,7 @@
 #include <queue>
 #include <vector>
 #include <numeric>
+#include <unordered_map>
 
 #include "Data.h"
 
@@ -16,9 +17,34 @@ class ScheduleEvaluator
 {
 private:
     const Data& data;
+    const Schedule& schedule;
+    // intersection -> {(green_light_start, green_light_end)} in the same street order as Schedule
+    std::unordered_map<int, std::vector<std::pair<int,int>>> streets_green_intervals;
 
-    void insert_starting_streets(std::vector<std::queue<std::pair<int, int>>>& street_to_queue, MinHeap& events)
+    std::vector<std::queue<std::pair<int, int>>> street_to_queue;
+    MinHeap events;
+
+    void precompute_street_green_intervals()
     {
+        int time = 0;
+        for (const auto& [intersection_id, street_schedule] : schedule)
+        {
+            auto& intervals = streets_green_intervals[intersection_id];
+            for (const auto [street_id, green_light_time] : street_schedule)
+            {
+                intervals.emplace_back(time, time + green_light_time - 1);
+                time += green_light_time;
+            }
+        }
+    }
+
+    // Modify, nu mai face event pt secunda 0 ca n-are sens
+    void insert_starting_streets()
+    {
+        // step 1: group cars by street
+        // step 2: using get_next_green_light_time and incrementng time, handle each street fully before moving on
+        // push new events to heap and to street_to_queue
+
         for (int car_id = 0; car_id < data.nr_cars; ++car_id)
         {
             const int starting_street = data.car_paths[car_id].front();
@@ -28,7 +54,31 @@ private:
         }
     }
 
-    std::pair<int, std::set<int>> get_street_ids_with_next_events(MinHeap& events)
+    int get_next_green_light_time(int street_id, int current_time)
+    {
+        const int intersection_id = std::get<0>(data.street_info.at(street_id));
+
+        const auto& street_schedule = schedule.find(intersection_id)->second;
+        auto it = std::find_if(street_schedule.begin(), street_schedule.end(),
+                               [street_id](const std::pair<int,int>& p) { return p.first == street_id; });
+        const int street_pos_in_sched = std::distance(street_schedule.begin(), it);
+
+        auto& green_intervals = streets_green_intervals[intersection_id];
+        const int streets_at_int = green_intervals.size();
+        const int cycle_length = green_intervals[streets_at_int - 1].second + 1;
+        const int sec_in_cycle = current_time % cycle_length;
+
+        const auto [desired_start, desired_end] = green_intervals[street_pos_in_sched];
+
+        if (desired_start <= sec_in_cycle && sec_in_cycle <= desired_end)
+            return current_time;
+        else if (desired_end < sec_in_cycle)
+            return current_time + cycle_length - sec_in_cycle + desired_start;
+        else
+            return desired_start - sec_in_cycle;
+    }
+
+    std::pair<int, std::set<int>> get_street_ids_with_next_events()
     {
         const int soonest_event_time = events.top().first;
         std::set<int> affected_street_ids;
@@ -39,36 +89,21 @@ private:
         }
         return {soonest_event_time, affected_street_ids};
     }
-
-    int get_waiting_time(int current_time, int street_id, int pos_in_queue, const Schedule& schedule)
-    {
-        const int intersection_id = std::get<0>(data.street_info.at(street_id));
-
-        // Always red light for this intersection
-        const auto& int_schedule_it = schedule.find(intersection_id);
-        if (int_schedule_it == schedule.end())
-            return -1;
-
-        const auto& int_schedule = int_schedule_it->second;
-        int cycle_length = std::accumulate(int_schedule.begin(), int_schedule.end(), 0,
-                                            [](int acc, const std::pair<int,int>& p) { return acc + p.second; });
-
-
-
-    }
 public:
 
-    explicit ScheduleEvaluator(const Data& data)
+    explicit ScheduleEvaluator(const Data& data, const Schedule& schedule)
     : data(data)
-    {}
-
-    int simulate(const Schedule& schedule)
+    , schedule(schedule)
     {
-        MinHeap events;
-        std::vector<int> car_id_to_street_pos(data.nr_cars);
-        std::vector<std::queue<std::pair<int, int>>> street_to_queue(data.nr_streets);
+        street_to_queue.reserve(data.nr_streets);
 
-        insert_starting_streets(street_to_queue, events);
+        precompute_street_green_intervals();
+        insert_starting_streets();
+    }
+
+    int simulate()
+    {
+        std::vector<int> car_id_to_street_pos(data.nr_cars);
 
         int t = 0;
         while (t <= data.simulation_seconds)
